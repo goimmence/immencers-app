b9725bab113a10a9b59229b42d56c3605ea98f06
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-01 15:02:25 +0530

    build: add container
    - Use Container, BoxDecoration, BorderRadious, BoxShadow

--- lib/widgets/container_sized.dart
+++ lib/widgets/container_sized.dart
@@ -10,11 +10,41 @@ class ContainerSized extends StatelessWidget {
         // backgroundColor: Colors.red,
         title: const Text("Container and SizedBox"),
       ),
-      body: const Center(
-        child: SizedBox(
-          height: 50,
-          width: 50,
-          child: Text("SizedBox"),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            // shape: BoxShape.circle,
+            // borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                spreadRadius: 5,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          child: const Center(
+            child: Text(
+              "Container",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          // const Center(
+          //   child: SizedBox(
+          //     height: 50,
+          //     width: 50,
+          //     child: Text("SizedBox"),
+          //   ),
+          // ),
         ),
       ),
     );

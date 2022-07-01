d12c87db45fcf2e006733d47335ebe0e0be7cb1f
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-01 12:56:51 +0530

    build: add material app with theme data
    - MaterialApp, ThemeData, Brightness, PrimaryColor

---
+++ lib/widgets/container_sized.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContainerSized extends StatelessWidget {
+  const ContainerSized({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

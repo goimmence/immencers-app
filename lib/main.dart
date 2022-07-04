5ec3ec1eb1b908c3a68bfa20d5b20d3ba82cfe35
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-04 14:15:06 +0530

    build: add snack bar
    - Use ElevatedButton, SnackBar, SnackBarBehavior, Border and Duration

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_basic/widgets/button.dart';
 import 'package:flutter_basic/widgets/container_and_sizedbox.dart';
 import 'package:flutter_basic/widgets/list_and_grid.dart';
 import 'package:flutter_basic/widgets/row_and_column.dart';
+import 'package:flutter_basic/widgets/snackbar.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: const ContainerAndSized();
       // home: const RowAndColumn(),
-      home: const ListAndGrid(),
+      home: const SnackBarWidget(),
     );
   }
 }
---
+++ lib/widgets/snackbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SnackBarWidget extends StatelessWidget {
+  const SnackBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Snackbar"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final snackBar = SnackBar(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              behavior: SnackBarBehavior.floating,
+              // padding: EdgeInsets.all(0),
+              duration: const Duration(
+                milliseconds: 3000,
+              ),
+              backgroundColor: Colors.red,
+              content: const Text("Snackbar here"),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          child: const Text("Snackbar"),
+        ),
+      ),
+    );
+  }
+}

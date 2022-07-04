b434f025bd1ad737b803f89b2b652eb0d678575a
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-04 11:36:22 +0530

    build: add grid view

--- lib/widgets/list_and_grid.dart
+++ lib/widgets/list_and_grid.dart
@@ -17,32 +17,55 @@ class _ListAndGridState extends State<ListAndGrid> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("List and Grid"),
-        backgroundColor: Colors.red,
-        elevation: 8,
-      ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: superHeros.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                onTap: () {
-                  debugPrint(superVillains["villains"][index]);
-                },
-                leading: const Icon(Icons.person),
-                title: Text(
-                  superVillains["heros"][index],
-                ),
-                subtitle: Text(
-                  superVillains["villains"][index],
+        appBar: AppBar(
+          title: const Text("List and Grid"),
+          backgroundColor: Colors.red,
+          elevation: 8,
+        ),
+        body: Container(
+          // child: ListView.builder(
+          //   itemCount: superHeros.length,
+          //   itemBuilder: (context, index) {
+          //     return Card(
+          //       child: ListTile(
+          //         onTap: () {
+          //           debugPrint(superVillains["villains"][index]);
+          //         },
+          //         leading: const Icon(Icons.person),
+          //         title: Text(
+          //           superVillains["heros"][index],
+          //         ),
+          //         subtitle: Text(
+          //           superVillains["villains"][index],
+          //         ),
+          //       ),
+          //     );
+          //   },
+          // ),
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                childAspectRatio: 2 / 4),
+            children: const [
+              Card(
+                child: Center(
+                  child: Text("Apple"),
                 ),
               ),
-            );
-          },
-        ),
-      ),
-    );
+              Card(
+                child: Center(
+                  child: Text("Apple"),
+                ),
+              ),
+              Card(
+                child: Center(
+                  child: Text("Apple"),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }

40e2d422383c8159bb2265a9a15286d31ac6e26c
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-05 11:31:17 +0530

    build: add cached network image
    - Add cached network image dependencies.
    - Fetch the network image and give the progress icon.

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
@@ -14,22 +15,31 @@ class ImageWidget extends StatelessWidget {
         child: Container(
           height: 200,
           width: 300,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 10,
-                spreadRadius: 5.0,
-                color: Colors.grey.shade800,
-              )
-            ],
-            image: const DecorationImage(
-              image: AssetImage(
-                "assets/batman.jpg",
-              ),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.red,
+
+          // decoration: BoxDecoration(
+          //   boxShadow: [
+          //     BoxShadow(
+          //       blurRadius: 10,
+          //       spreadRadius: 5.0,
+          //       color: Colors.grey.shade800,
+          //     )
+          //   ],
+          //   image: const DecorationImage(
+          //     image: AssetImage(
+          //       "assets/batman.jpg",
+          //     ),
+          //     fit: BoxFit.cover,
+          //   ),
+          //   borderRadius: BorderRadius.circular(20),
+          //   color: Colors.red,
+          // ),
+
+          child: CachedNetworkImage(
+            imageUrl:
+                "https://cdn.mos.cms.futurecdn.net/m6txc3A6HUa22VqX5QGALc.jpg",
+            placeholder: (context, url) =>
+                const Center(child: CircularProgressIndicator()),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
           ),
         ),
       ),

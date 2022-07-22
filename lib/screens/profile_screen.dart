d141b4c5e7163b1020e24793ddef359c945a01fb
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-22 10:11:48 +0530

    build: add app icon and change this name

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,6 +2,8 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/utils/app_layout.dart';
 import 'package:flutter_basic/utils/app_styles.dart';
+import 'package:flutter_basic/widgets/small_dash_line_widget.dart';
+import 'package:flutter_basic/widgets/ticket_details.dart';
 import 'package:gap/gap.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -18,7 +20,9 @@ class ProfileScreen extends StatelessWidget {
         ),
         children: [
           Gap(AppLayout.getHeight(40)),
+          /* -------------------------------- App icon -------------------------------- */
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 height: AppLayout.getHeight(86),
@@ -30,6 +34,7 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ),
               Gap(AppLayout.getHeight(10)),
+              // /* ------------------------------ Book Tickets ------------------------------ */
               Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -41,7 +46,7 @@ class ProfileScreen extends StatelessWidget {
                     style: Styles.body1.copyWith(color: Styles.grayColor),
                   ),
                   Gap(AppLayout.getHeight(8)),
-                  /* ----------------------------- Premium Status ----------------------------- */
+                  //     /* ----------------------------- Premium Status ----------------------------- */
                   Container(
                     padding: EdgeInsets.symmetric(
                       horizontal: AppLayout.getHeight(3),
@@ -75,12 +80,213 @@ class ProfileScreen extends StatelessWidget {
                           style:
                               Styles.body1.copyWith(color: Styles.primaryColor),
                         ),
+                        Gap(AppLayout.getHeight(5)),
                       ],
                     ),
                   ),
                 ],
               ),
+              const Spacer(),
+              Column(
+                // crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      debugPrint("You tapped");
+                    },
+                    child: Text("Edit",
+                        style:
+                            Styles.body1.copyWith(color: Styles.primaryColor)),
+                  ),
+                ],
+              ),
             ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(
+            color: Styles.grayColor,
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Stack(
+            children: [
+              Container(
+                height: AppLayout.getHeight(90),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Styles.primaryColor,
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(18))),
+              ),
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(
+                    AppLayout.getHeight(30),
+                  ),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 18,
+                      color: const Color(0xFF1B73B1),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(25),
+                    vertical: AppLayout.getHeight(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: Styles.primaryColor,
+                        size: 27,
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("You got a new award",
+                            style:
+                                Styles.heading5.copyWith(color: Colors.white)),
+                        Text("You have 150 flights in a year",
+                            style: Styles.body1.copyWith(color: Colors.white))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(25)),
+          Text(
+            "Accumulated miles",
+            style: Styles.heading4,
+          ),
+          Gap(AppLayout.getHeight(25)),
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: AppLayout.getHeight(20),
+                vertical: AppLayout.getHeight(20)),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                AppLayout.getHeight(18),
+              ),
+              color: Colors.white,
+              // boxShadow: const [
+              //   BoxShadow(
+              //     color: Colors.grey,
+              //     blurRadius: 0.3,
+              //     spreadRadius: 0.3,
+              //   ),
+              // ],
+            ),
+            child: Column(
+              children: [
+                Gap(AppLayout.getHeight(20)),
+                Text("192802", style: Styles.heading3),
+                Gap(AppLayout.getHeight(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Miles accrued",
+                      style: Styles.body1.copyWith(color: Styles.grayColor),
+                    ),
+                    Text(
+                      "23 May 2021",
+                      style: Styles.body1.copyWith(color: Styles.grayColor),
+                    ),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(4)),
+                Divider(
+                  color: Styles.grayColor,
+                ),
+                Gap(AppLayout.getHeight(4)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    TicketDetails(
+                      firstText: "23 042",
+                      secondText: "Miles",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    TicketDetails(
+                      firstText: "Airline CO",
+                      secondText: "Received from",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(12)),
+                const SmallDashLineWidget(
+                  sections: 12,
+                  isColor: true,
+                ),
+                Gap(AppLayout.getHeight(12)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    TicketDetails(
+                      firstText: "24",
+                      secondText: "Miles",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    TicketDetails(
+                      firstText: "McDonal's",
+                      secondText: "Received from",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(12)),
+                const SmallDashLineWidget(
+                  sections: 12,
+                  isColor: true,
+                ),
+                Gap(AppLayout.getHeight(12)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    TicketDetails(
+                      firstText: "52 340",
+                      secondText: "Miles",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    TicketDetails(
+                      firstText: "Exuma",
+                      secondText: "Received from",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Gap(AppLayout.getHeight(12)),
+          Center(
+            child: Text(
+              "How to get more miles",
+              style: Styles.body1.copyWith(
+                color: Styles.primaryColor,
+              ),
+            ),
           ),
         ],
       ),

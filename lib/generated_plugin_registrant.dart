ed7cb89f6aa66b73582a057bda4a846cac221cdd
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-07-26 10:12:35 +0530

    chore: add firebase core and database dependency

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,12 +6,16 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }

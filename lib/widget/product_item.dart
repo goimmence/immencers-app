afe0c53346c4f18f2a3e9267f0b2394bf7708458
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-08-08 16:13:14 +0530

    build: add route for product details screen

--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screen/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
 
-  ProductItem({
+  const ProductItem({
+    Key? key,
     required this.id,
     required this.title,
     required this.imageUrl,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black45,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ProductDetailsScreen(
+                      title: title,
+                    )));
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }

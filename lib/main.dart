2539ca7e9026c7d9fc114cbeb49d465fa1e6f4ce
Brijeshkumar Chavda <brijesh.c@immence.in>
2022-08-18 18:18:29 +0530

    fix: flutter lints issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/screens/auth/login_screen.dart';
 import 'package:flutter_basic/screens/auth/signup_screen.dart';
-import 'package:flutter_basic/screens/wrapper.dart';
 import 'package:flutter_basic/utils/app_styles.dart';
 
 Future main() async {
